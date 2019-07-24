0c27346eb6c61da3d96535213e5dd2631940e6f8
Jônatas Santos <jonatas.santos@aedu.com>
2019-07-24 17:16:03 -0300

    Configurado as entidades do projeto, bem como as cardinalidades. E adicionado o arquivo data.sql para popular o banco H2.

--- forum/src/main/java/br/com/jonatas/forum/Model/Resposta.java
+++ forum/src/main/java/br/com/jonatas/forum/Model/Resposta.java
@@ -1,13 +1,19 @@
 package br.com.jonatas.forum.Model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
@@ -83,5 +89,4 @@ public class Resposta {
 	public void setSolucao(Boolean solucao) {
 		this.solucao = solucao;
 	}
-
 }
--- forum/src/main/java/br/com/jonatas/forum/Model/Usuario.java
+++ forum/src/main/java/br/com/jonatas/forum/Model/Usuario.java
@@ -1,7 +1,16 @@
 package br.com.jonatas.forum.Model;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
@@ -63,5 +72,4 @@ public class Usuario {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-
 }
